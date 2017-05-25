c54aa957ccea9413d5591671badc3e4951c97778
Nicholas Chung <nich.chung@gmail.com>
2017-05-24 21:40:17 -0400

    homepage animations tests

---
+++ static/js/portrait-basic.js
@@ -0,0 +1,7 @@
+var portrait = Snap.select("#portraitSVG");
+var eyeLeft = portrait.select("#eyeball-left");
+eyeLeft.attr({
+    fill: "#addedd",
+    strokeWidth: 5,
+    stroke: "#beeeef"
+});
---
+++ static/js/portrait.js
@@ -0,0 +1,39 @@
+(function () {
+    var portrait = Snap.select("#portraitSVG");
+    var eyeRight = portrait.select("#eyeball-right");
+    var eyeLeft = portrait.select("#eyeball-left");
+    lineLeft = portrait.line(
+        219.141,
+        447.36,
+        eyeLeft.x,
+        eyeLeft.y
+    ).attr({
+        stroke: "#ffff00",
+        strokeWidth: 5,
+        strokeLinecap: "round",
+        strokeDasharray: "10 20 10 20"
+    });
+    lineRight = portrait.line(
+        489.519,
+        450.149,
+        eyeRight.x,
+        eyeRight.y
+    ).attr({
+        stroke: "#ffff00",
+        strokeWidth: 5,
+        strokeLinecap: "round",
+        strokeDasharray: "10 20 10 20"
+    });
+    portrait.mousemove(function (event) {
+        lineLeft.attr({
+            x2: event.pageX,
+            y2: event.pageY
+        });
+    });
+    portrait.mousemove(function (event) {
+        lineRight.attr({
+            x2: event.pageX,
+            y2: event.pageY
+        });
+    });
+})();
